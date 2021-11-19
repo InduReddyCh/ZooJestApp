20c3bdba8978efd92d088f74c4fd919ff8e7fba6
InduReddyCh <89427709+InduReddyCh@users.noreply.github.com>
2021-11-19 11:56:49 -0600

    added exceptions

--- src/main/java/edu/nwmissouri/zoo06group/Hippopotamus.java
+++ src/main/java/edu/nwmissouri/zoo06group/Hippopotamus.java
@@ -5,6 +5,9 @@
  */
 package edu.nwmissouri.zoo06group;
 
+import java.io.FileNotFoundException;
+import java.lang.invoke.StringConcatException;
+
 /**
  *
  * @author Indu Chinthakuntla
@@ -33,6 +36,45 @@ public class Hippopotamus extends Animal {
         double c = getHippopotamusAddition(a, b);
         System.out.printf("I know HippopotamusAddition! %4.2f plus %d is %4.2f \n", a, b, c);
     }
+    
+    public void divide()throws ArithmeticException{
+        try{
+          int a=2;
+          int b=0;
+          System.out.println(a/b);  
+        }
+        catch(ArithmeticException e){
+            System.out.println("throws an exception");
+        }
+        finally{
+            System.out.println("Handling an exception");
+        }
+        
+    }
+    public void multipy()throws ArrayIndexOutOfBoundsException{
+        try{
+            int[] array={1,2,3,4};
+            System.out.println("Multiplication of two numbers"+array[3]*array[4]);
+        }
+        finally{
+            System.out.println("handling array exception");
+        }
+    }
+    public void name() {
+        try{
+            String s1="Indu";
+            for(int i=0;i<s1.length();i++)
+            System.out.println("character at 10"+s1.charAt(10));
+        }
+        catch(StringIndexOutOfBoundsException e){
+            System.out.println("exception");
+        }
+        finally{
+            System.out.println("string exception");
+        }
+        
+        
+    }
 
     /**
      * Custom Hippopotamus function - your class must use a unique function name
@@ -52,10 +94,16 @@ public class Hippopotamus extends Animal {
         a.speak();
         a.move();
         a.profess();
+        a.divide();
+        a.name();
+        a.multipy();
+       
     }
 
 }
 
 
     
+
+
 
